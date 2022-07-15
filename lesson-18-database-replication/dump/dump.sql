31b9308209b0d9d2bdcd28a6da5881f37a2a8034
aleksey.shumlyaev <aleksey.shumlyaev@rhinocodes.com>
2022-07-15 15:35:16 +0300

    hmw-18 removed unused files

--- lesson-18-database-replication/dump/dump.sql
+++
@@ -1,8 +0,0 @@
-USE home-work-18;
-
-CREATE TABLE IF NOT EXIST `users` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `birth_date` double DEFAULT NULL,
-  `age` double DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
