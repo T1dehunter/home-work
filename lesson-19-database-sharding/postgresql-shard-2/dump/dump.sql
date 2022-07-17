4a5d447775b1d301d43c5c07cafd03377b7ec0e1
alexey.shumlyaev <aleksey.shumlyaev@rhinocodes.com>
2022-07-18 00:14:05 +0300

    hmw-19 added second shard

--- lesson-19-database-sharding/postgresql-shard-2/dump/dump.sql
+++ lesson-19-database-sharding/postgresql-shard-2/dump/dump.sql
@@ -5,3 +5,7 @@ CONSTRAINT category_id_check CHECK ( category_id = 2 ),
 author character varying not null,
 title character varying not null,
 year int not null );
+
+CREATE EXTENSION postgres_fdw;
+
+CREATE SERVER books_base FOREIGN DATA WRAPPER postgres_fdw OPTIONS(host '127.0.0.1', port '5432', dbname 'books');
