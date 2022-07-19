a068e9a59113cd35950e55f2fb741eb859d7d996
aleksey.shumlyaev <aleksey.shumlyaev@rhinocodes.com>
2022-07-19 11:34:55 +0300

    hmw-19 fixed shards settings

--- lesson-19-database-sharding/postgresql-shard-2/dump/dump.sql
+++ lesson-19-database-sharding/postgresql-shard-2/dump/dump.sql
@@ -1,6 +1,6 @@
 CREATE TABLE books (
 id bigint not null,
-category_id  int not null,
+category_id int not null,
 CONSTRAINT category_id_check CHECK ( category_id = 2 ),
 author character varying not null,
 title character varying not null,
